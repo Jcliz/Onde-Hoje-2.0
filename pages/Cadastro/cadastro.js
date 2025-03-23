4ffecdc50df8ac61f0e7e2227be5e40023a10097
Jcliz <joaopedrocliz@gmail.com>
2025-03-23 19:09:38 -0300

    verificação de CPF

--- pages/Cadastro/cadastro.js
+++ pages/Cadastro/cadastro.js
@@ -4,6 +4,7 @@ const bairro = document.querySelector('#bairro');
 const cidade = document.querySelector('#cidade');
 const numero = document.querySelector('#number');
 const mensagem = document.querySelector('#mensagem');
+const cpf = document.getElementById('cpf').value;
 
 function validarSenha() {
   console.log('chamou a validação');
@@ -140,16 +141,18 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-cep.addEventListener('focusout', async () => {
+//verificação de CEP e auto completamento
+async function validarCEP(input) {
   const onlyNumbers = /^[0-9]+$/;
   const cepValid = /^[0-9]{8}$/;
+  const cepError = input.nextElementSibling; 
 
   try {
-    if (!onlyNumbers.test(cep.value) || !cepValid.test(cep.value)) {
-      throw { cep_error: 'CEP inválido' };
+    if (!onlyNumbers.test(input.value) || !cepValid.test(input.value)) {
+      throw { cep_error: 'CEP inválido.' };
     }
 
-    const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+    const response = await fetch(`https://viacep.com.br/ws/${input.value}/json/`);
 
     if (!response.ok) {
       throw await response.json();
@@ -159,15 +162,50 @@ cep.addEventListener('focusout', async () => {
 
     rua.value = responseCep.logradouro;
     bairro.value = responseCep.bairro;
-    cidade.value = responseCep.localidade;
+    cidade.value = `${responseCep.localidade} - ${responseCep.uf}`;
+
+    input.classList.remove('is-invalid');
+    cepError.style.display = 'none';
 
   } catch (error) {
     if (error?.cep_error) {
-      mensagem.textContent = error.cep_error;
-      setTimeout(() => {
-        mensagem.textContent = "Por favor, insira um CEP válido.";
-      }, 5000);
+      input.classList.add('is-invalid');
+      cepError.style.display = 'block';
+      cepError.textContent = error.cep_error;
     }
+    
     console.log(error);
   }
-})
+}
+
+cep.addEventListener('focusout', async () => {
+  await validarCEP(cep);
+});
+
+//verificação de CPF
+function validarCPF(input) {
+  const cpfError = input.nextElementSibling; 
+  const cpfValid = /^[0-9]{11}$/; 
+
+  input.value = input.value.replace(/\D/g, '');
+
+  if (cpfValid.test(input.value)) {
+    input.value = input.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+
+    input.classList.remove('is-invalid');
+    cpfError.style.display = 'none';
+  } else {
+    input.classList.add('is-invalid');
+    cpfError.style.display = 'block';
+  }
+}
+
+document.querySelectorAll('#cpf').forEach(cpfInput => {
+  cpfInput.addEventListener('input', () => {
+    cpfInput.value = cpfInput.value.replace(/\D/g, '');
+  });
+
+  cpfInput.addEventListener('focusout', () => {
+    validarCPF(cpfInput);
+  });
+});
